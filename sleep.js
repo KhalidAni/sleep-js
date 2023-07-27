a4e02dcc0ba73bbf81dcd0826836c353a2337dba
khalidani <walemash3@gmail.com>
2023-07-27 08:06:03 +0100

    sleep.js assignment

---
+++ sleep.js
@@ -0,0 +1,56 @@
+const getSleepHours = (day) => {
+  const getSleepHours = (day) => {
+    if (day === "monday") {
+      return 8;
+    } else if (day === "tuesday") {
+      return 7;
+    } else if (day === "wednesday") {
+      return 9;
+    }
+  };
+
+  console.log(getSleepHours("monday"));
+  console.log(getSleepHours("tuesday"));
+  console.log(getSleepHours("wednesday"));
+
+  const getActualSleepHours = () => {
+    return (
+      getSleepHours("monday") +
+      getSleepHours("tuesday") +
+      getSleepHours("wednesday") +
+      getSleepHours("thursday") +
+      getSleepHours("friday") +
+      getSleepHours("saturday") +
+      getSleepHours("sunday")
+    );
+  };
+
+  const getIdealSleepHours = () => {
+    const idealHours = 8;
+    return idealHours * 7;
+  };
+
+  console.log(getActualSleepHours());
+  console.log(getIdealSleepHours());
+
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+
+    if (actualSleepHours === idealSleepHours) {
+      console.log("You got the perfect amount of sleep.");
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log(
+        `You got ${
+          actualSleepHours - idealSleepHours
+        } hour(s) more sleep than needed.`
+      );
+    } else {
+      console.log(
+        `You should get some rest. You got ${
+          idealSleepHours - actualSleepHours
+        } hour(s) less sleep than you needed this week.`
+      );
+    }
+  };
+};
